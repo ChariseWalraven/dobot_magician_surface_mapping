c9e84e46f98f01cfdbf526d25b15f8fa96c7a610
Charise Walraven <31189070+ChariseWalraven@users.noreply.github.com>
2025-02-06 14:57:34 +0100

    First working version initial scan data collection

--- dobot_lab_code.py
+++ dobot_lab_code.py
@@ -6,31 +6,37 @@ import time
 import json
 import numpy as np
 import matplotlib as plt
+import threading
 
+from datetime import datetime
 # check ports automatically - not needed, we can set it manually to the correct port,
 # but it is helpful to check which port is correct
 # ports = list(serial.tools.list_ports.comports())
 
 #print([(p.device, p.description) for p in ports])\
-run_loop = False
 
-r_const = 0
-z_home = 100
-z_sweep = 50
+z_sweep = -9
+file_content = {
+  "measurements": [],
+  "coordinates": []
+}
+
+location = "C:\\Users\\Gebruiker\\Desktop\\scanner_data\\"
 
 # set initial position
 # scan in steps, saving coordinates and measured distance to a list
 # plot list at end of scan
 coordinates = {
-  "bottom_right": (90, -140),
-  "bottom_left": (112, -98),
-  "bottom_middle": (130, -20),
-  "top_right": (283, -164),
-  "top_left": (316, 78),
-  "top_middle": (312, -46),
-  "right_middle": (179, -156),
-  "left_middle": (221, 91),
+  "bottom_right": (172, -13, z_sweep, 0),
+  "top_left": (146, 292, z_sweep, 0),
+  "bottom_left": (19, 192, z_sweep, 88),
+  "top_right": (294, 80, z_sweep, 18.9),
+  "top_middle": (225, 188, z_sweep, 71),
+  "bottom_middle": (98, 88, -10, 46),
+  "right_middle": (241, 26, z_sweep, 11),
+  "left_middle": (82, 242, z_sweep, 76),
 }
+
 
 arduino = serial.Serial(port='COM4', baudrate=9600, timeout=0.1)
 
@@ -39,15 +45,43 @@ def print_pose():
   print( magician.get_pose())
  
 
+def setup():
+  #set speed and acceleration to 50%
+  magician.motion_params(50, 50)
+
+
 def go_home():
-  magician.ptp(mode=0, x=200, y=0, z=z_home,  r=r_const)
+  magician.ptp(mode=0, x=200, y=0, z=100,  r=0)
+
 
 def do_initial_sweep():
   # br - tl - bl - tr - tm - bm - rm - lm
-  for c_name, c_vals in coordinates.items():
-    print(c_name, c_vals)
-    magician.ptp(mode=0, x=c_vals[0], y=c_vals[1], z=z_sweep, r=r_const)
-    time.sleep(0.5)
+  mode = 2
+  byte_size = 500
+
+  def measure():
+    print('Measuring')
+    while moving:
+      data = {}
+      data['arduino'] = arduino.readline().decode("utf-8")
+      data['time'] = str(time.time())
+      file_content["measurements"].append(data)
+      time.sleep(0.5)
+
+  def move(x,y,z,r, dest):
+    print("Moving to:",  dest)
+    magician.ptp(mode=mode, x=x, y=y, z=z, r=r)
+    moving = False
+
+  
+  for key, coord in coordinates.items():
+    t = threading.Thread(target=measure)
+    moving = True
+    t.start()
+    file_content["coordinates"].append({ "pose": magician.get_pose(), "time":  str(time.time()) })
+    move(*coord, key)
+    moving = False
+    t.join()
 
 def read_json(data: bytes):
   # print("data: ", data)
@@ -56,12 +90,10 @@ def read_json(data: bytes):
 
 
 # main code
-go_home()
-time.sleep(0.5)
-#doinitial_sweep()
+setup()
+do_initial_sweep()
 
-while run_loop:
-  time.sleep(0.01)
-  data = read_json(arduino.readline())
-  print("mm: ", data['distance_mm'])
-  time.sleep(0.5)
+dt_str = datetime.now().strftime("%d_ %m_%Y_%H_%M_%S")
+filename = f"{location}data{dt_str}.json"
+with open(filename, "w+") as f:
+  f.write(json.dumps(file_content))
