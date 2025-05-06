9af4246e2459e14448428e5c87cb9a98f8e8b705
Charise Walraven <31189070+ChariseWalraven@users.noreply.github.com>
2025-05-06 16:16:16 +0200

    Merges some code from manual_test.py

--- dobot_lab_code.py
+++ dobot_lab_code.py
@@ -1,5 +1,4 @@
 # version: Python3
-from typing import Tuple
 from DobotEDU import *
 import serial.tools.list_ports
 import time
@@ -7,20 +6,10 @@ import json
 import threading
 
 from datetime import datetime
-# check ports automatically - not needed, we can set it manually to the correct port,
-# but it is helpful to check which port is correct
-# ports = list(serial.tools.list_ports.comports())
 
-#print([(p.device, p.description) for p in ports])\
-
+# CONSTANTS
 z_sweep = -20
-file_content = []
-
 location = "C:\\Users\\Gebruiker\\Desktop\\scanner_data"
-
-# set initial position
-# scan in steps, saving coordinates and measured distance to a list
-# plot list at end of scan
 coordinates = {
     "bottom_right": (141, -126, z_sweep, -42),
     "top_left": (269, 155, z_sweep, 30),
@@ -31,9 +20,6 @@ coordinates = {
     "right_middle": (206, -129, z_sweep, -32),
     "left_middle": (197, 151, z_sweep, 37),
 }
-
-
-arduino = serial.Serial(port='COM4', baudrate=9600, timeout=0.1)
 
 
 def print_pose():
@@ -51,14 +37,17 @@ def go_home():
     magician.ptp(mode=0, x=200, y=0, z=z_sweep,  r=0)
 
 
-def do_initial_sweep():
+def do_initial_sweep(arduino: serial.Serial) -> list:
+    file_content = []
     # br - tl - bl - tr - tm - bm - rm - lm
     mode = 2
     origin = {
         'home': magician.get_pose()
     }
 
-    def measure(dest, origin):
+    # TODO: read all lines after moving one leg to get all data instead of going line per line
+    # TODO: replace the code below with the code in manual_test.py so that the output for both data files is the same
+    def measure(dest: dict, origin: dict, arduino: serial.Serial):
         """Measure distance while moving"""
         print('Measuring')
         while moving:
@@ -69,18 +58,18 @@ def do_initial_sweep():
                 'time': str(time.time())
             }
             file_content.append(data)
-            time.sleep(0.5)  # NOTE: this is 500 ms not 50, so we're getting less measurements than the arduino sends to the serial port
+            # delay for 50ms
+            time.sleep(0.05)
         print('Done measuring')
 
-    def move(x,y,z,r, dest):
+    def move(x, y, z, r, dest: dict):
         print("Moving to:",  dest)
         magician.ptp(mode=mode, x=x, y=y, z=z, r=r)
 
     for key, coord in coordinates.items():
-        dest = {}
-        dest[key] = coord
+        dest = {key: coord}
         # Start a thread so we can read sensor data and move at the same time
-        t = threading.Thread(target=measure, args=(dest, origin))
+        t = threading.Thread(target=measure, args=(dest, origin, arduino))
         moving = True  # Needed for measure function (above), do not delete
         t.start()
         move(*coord, key)
@@ -89,18 +78,50 @@ def do_initial_sweep():
 
         origin = {}  # <- not sure why I did this, if the program runs the same without it, then it can be deleted.
         origin = dest
+    return file_content
+
+def connect_to_arduino() -> serial.Serial:
+    arduino_ports = []
+
+    print('Finding arduino port...')
+    while len(arduino_ports) <= 0:
+        ports = list(serial.tools.list_ports.comports())
+
+        arduino_ports = [
+            p.device
+            for p in serial.tools.list_ports.comports()
+            if 'IOUSBHostDevice' in p.description
+        ]
+
+        time.sleep(0.5)
+
+    print('found port, connecting...')
+    port = arduino_ports[0]
+    arduino = serial.Serial(port=port, baudrate=9600, timeout=0.1)
+    print('Connected.')
+    return arduino
+
+
+def write_data(dir_location: str, file_content, should_get_user_comment = False):
+    dt_str = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+    filename = f"{dir_location}/data_{dt_str}.json"
+
+    if should_get_user_comment:
+        comment = input('Enter comment for top of file:')
+        file_content['comment'] = comment
+
+    with open(filename, "w+") as f:
+        f.write(json.dumps(file_content))
+
+
+def main():
+    setup()
+    go_home()
+    arduino = connect_to_arduino()
+    file_content = do_initial_sweep(arduino)
+    go_home()
+    write_data(location, file_content, should_get_user_comment=False)
 
 
 if __name__ == '__main__':
-    setup()
-
-    go_home()
-    do_initial_sweep()
-    go_home()
-
-    dt_str = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-    filename = f"{location}/data_{dt_str}.json"
-
-    # write data to scanner data folder (see location at top of file).
-    with open(filename, "w+") as f:
-        f.write(json.dumps(file_content))
+    main()
