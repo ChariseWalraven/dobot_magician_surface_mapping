8fa5839d0f3b31c173fb0410d73d4260ee3a5f8e
Charise Walraven <31189070+ChariseWalraven@users.noreply.github.com>
2025-05-20 15:16:02 +0200

    fwv batch readlines; adds comment functionality

--- dobot_lab_code.py
+++ dobot_lab_code.py
@@ -22,6 +22,7 @@ coordinates = {
   "left_middle": (197, 151, z_sweep, 37),
 }
 test_batch_sweep = True
+should_get_user_comment = True
 
 
 def print_pose():
@@ -36,7 +37,7 @@ def setup():
 
 def go_home():
   """Go to the home position"""
-  magician.ptp(mode=0, x=200, y=0, z=z_sweep,  r=0)
+  magician.ptp(mode=0, x=200, y=0, z=z_sweep, r=0)
 
 
 def move(x, y, z, r, dest_name: dict, mode=2):
@@ -58,12 +59,14 @@ def parse_data(data):
 
 
 def do_initial_sweep_batch(arduino: serial.Serial) -> list:
-  file_content = []
+  file_content = {
+    "comment": None,
+    "measurements": None,
+  }
   # br - tl - bl - tr - tm - bm - rm - lm
   origin = {
     'home': magician.get_pose()
   }
-
 
   def measure(dest: dict, origin: dict, arduino: serial.Serial):
     """Measure distance while moving"""
@@ -78,7 +81,7 @@ def do_initial_sweep_batch(arduino: serial.Serial) -> list:
     # dest_dict = { str(dest_name):  list(coords)}
 
     measurements = []
-    
+
     print('reading lines')
     data = arduino.readlines()
     print('done reading lines')
@@ -98,11 +101,8 @@ def do_initial_sweep_batch(arduino: serial.Serial) -> list:
       measurements.append(measurement)
       print('measurement appended, done with this line of data')
 
-
     print('Done measuring')
     return measurements
-
-
 
   for leg_name, coord in coordinates.items():
     dest = {leg_name: coord}
@@ -115,7 +115,7 @@ def do_initial_sweep_batch(arduino: serial.Serial) -> list:
     # it would be a better idea to send timestamps with the measurements from the Arduino.
     measurements = measure(dest, origin, arduino)
 
-    file_content.append(*measurements)
+    file_content['measurements'] = measurements
 
     origin = {}  # <- not sure why I did this, if the program runs the same without it, then it can be deleted.
     origin = dest
@@ -145,7 +145,7 @@ def do_initial_sweep(arduino: serial.Serial) -> list:
       print(f'Moving to {dest_name} from {origin_name}')
       print('coords:', coords)
 
-      dest_dict = { str(dest_name):  list(coords)}
+      dest_dict = {str(dest_name): list(coords)}
 
       # parse data
       re_str = r"(\w*:\d{1,5})"
@@ -196,15 +196,16 @@ def connect_to_arduino() -> serial.Serial:
     print('No ports found yet, try count:', try_count)
 
     ports = list(serial.tools.list_ports.comports())
-    print('ports:',  [
-      ( p.device, p.description)
+    print('ports:', [
+      (p.device, p.description)
       for p in serial.tools.list_ports.comports()
     ])
 
     arduino_ports = [
       p.device
       for p in serial.tools.list_ports.comports()
-      if 'IOUSBHostDevice' in p.description or 'Serieel USB-apparaat' in p.description  # This is apparently different per OS/language. Can it be set?
+      if 'IOUSBHostDevice' in p.description or 'Serieel USB-apparaat' in p.description
+      # This is apparently different per OS/language. Can it be set?
     ]
 
     try_count += 1
@@ -212,12 +213,12 @@ def connect_to_arduino() -> serial.Serial:
 
   print('found port, connecting...')
   port = arduino_ports[0]
-  arduino = serial.Serial(port=port, baudrate=9600, timeout=1)
+  arduino = serial.Serial(port=port, baudrate=9600, timeout=0.005)
   print('Connected.')
   return arduino
 
 
-def write_data(dir_location: str, file_content, should_get_user_comment = False):
+def write_data(dir_location: str, file_content, should_get_user_comment=False):
   dt_str = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
   filename = f"{dir_location}/data_{dt_str}.json"
 
@@ -225,8 +226,8 @@ def write_data(dir_location: str, file_content, should_get_user_comment = False)
     comment = input('Enter comment for top of file:')
     file_content['comment'] = comment
 
-  with open(filename, "w+",  encoding="utf-8") as f:
-    f.writelines(json.dumps(file_content, indent=2,  ensure_ascii=False))
+  with open(filename, "w+", encoding="utf-8") as f:
+    f.writelines(json.dumps(file_content, indent=2, ensure_ascii=False))
 
 
 def main():
@@ -238,7 +239,7 @@ def main():
   else:
     file_content = do_initial_sweep(arduino)
   go_home()
-  write_data(location, file_content, should_get_user_comment=False)
+  write_data(location, file_content, should_get_user_comment=should_get_user_comment)
 
 
 if __name__ == '__main__':
